c9bf8ee3321e6b7b00ecb71f85420a39753ac16a
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-06-07 09:37:44 -0300

    feat: first configs

---
+++ db.js
@@ -0,0 +1,17 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pg;
+
+const dbConfig = ({
+  connectionString: process.env.DATABASE_URL
+});
+
+if (process.env.MODE === "PROD") {
+  dbConfig.ssl = {
+    rejectUnauthorized: false
+  }
+};
+
+const db = new Pool(dbConfig);
