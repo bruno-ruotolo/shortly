23f65a56edeb9f7892a9aeaa904e114d5ca81c74
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-06-10 19:39:10 -0300

    fix: authSchema[

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -10,5 +10,5 @@ export const signUpSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\d)[A-Za-z\d]{6,}$/).required(),
-  confirmPassword: joi.ref("password")
+  confirmPassword: joi.string().valid(joi.ref("password")).required()
 });
